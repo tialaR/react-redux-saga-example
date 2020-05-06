25f5a1a9ba5e3d183ecad8f370674c407c4f35a6
Tiala <tialarocha@Tialas-MacBook-Pro.local>
2020-05-06 19:24:13 -0300

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import todos from './reducer';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({
+    todos,
+  }),
+  applyMiddleware(sagaMiddleware),
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
---
+++ src/store/sagas.js
@@ -0,0 +1,28 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+
+function apiGet() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, text: 'Fazer café 1' },
+        { id: 2, text: 'Fazer café 2' },
+        { id: 3, text: 'Fazer café 3' },
+        { id: 4, text: 'Fazer café 4' },
+      ]);
+    }, 2000);
+  });
+}
+
+function* getTodoList() {
+  try {
+    const response = yield call(apiGet);
+
+    yield put({ type: 'SUCCESS_TODO_LIST', payload: { data: response } });
+  } catch (err) {
+    yield put({ type: 'FAILURE_TODO_LIST' });
+  }
+}
+
+export default function* root() {
+  yield takeLatest('REQUEST_TODO_LIST', getTodoList);
+}
